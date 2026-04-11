f704ed62ed661f9ca63160f340b26bc904e90d0e
Jakub Pawlina <jakub.pawlina@outlook.com>
2026-04-11 09:28:49 +0200

    Lab3 - solution

--- materialy/lab_03/conftest.py
+++ materialy/lab_03/conftest.py
@@ -14,3 +14,31 @@ from calculator import Calculator
 def calc():
     """Fixture tworzacy instancje Calculator -- dostepny globalnie."""
     return Calculator()
+
+
+def pytest_addoption(parser):
+    """Dodaje opcje wlaczajaca testy zadania dodatkowego."""
+    parser.addoption(
+        "--run-additional",
+        action="store_true",
+        default=False,
+        help="uruchom testy oznaczone jako additional",
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    """Domyslnie odfiltrowuje testy zadania dodatkowego."""
+    if config.getoption("--run-additional"):
+        return
+
+    selected_items = []
+    deselected_items = []
+    for item in items:
+        if "additional" in item.keywords:
+            deselected_items.append(item)
+        else:
+            selected_items.append(item)
+
+    if deselected_items:
+        config.hook.pytest_deselected(items=deselected_items)
+        items[:] = selected_items
